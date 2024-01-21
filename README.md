a0c3cb19f0c9e20bfc3923cbf808adf760537912
dustein <steindu@gmail.com>
2024-01-20 22:11:51 -0300

    retificando

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Projeto backend loja virtual propaganda curso Python da Rocketseat.
 Utilizando Flask e SQLite.
 
 - Instalando dependências. Ou utiliza o arquivo requirements.txt ("pip3 install -r requirements.txt") ou "pip3 install Flask==2.3.0" por exemplo.
+
+- Depois da modelagem, vamos criar o banco de dados.
+No terminal: "flask shell" para abrir o prompt de comando, então "db.create_all()" e sera criada o bando de dados conforme a tabela de modelagem. Após o comando, se não tiver nenhuma mensagem de retorno, é porque não deu erro e o banco foi criado.
+  Em seguida, "db.session.commit()". A session é a propriedade do db que armazena a conexão com o bando de dados, e o método commit é o que vai efetivar as mudanças feitas na tabela, se não der commit, não enviará a mudança.
+  Em seguida, "exit()"
+
+  
--- app.py
+++ app.py
@@ -4,10 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #definir caminho do banco de dados
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://ecommerce.db'
 #iniciar conexao com o banco de dados
 db = SQLAlchemy(app)
-#Modelagem do banco em linhas e colunas
+#Modelagem do banco em linhas e colunas (id,name,price,description)
+class Product(db.Model):
+  # o id sera um numero inteiro, e sera a chave unica
+  id = db.Column(db.Integer, primary_key=True)
+  # o name sera uma string de ate 120 caracteres, nao podendo ser nulo/nao informado 
+  name = db.Column(db.String(120), nullable=False)
+  #  price sera um numero real, tambem nao pode ser nulo
+  price = db.Column(db.Float, nullable=False)
+  # a description usamos um tipo Texto, que nao tem limitacao de tamanho
+  description = db.Column(db.Text, nullable=True)
 
 
 
