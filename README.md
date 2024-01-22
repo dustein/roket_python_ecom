3a691e4a73267f9fd2665838b93e761a27d13c77
dustein <steindu@gmail.com>
2024-01-22 19:51:47 -0300

    criando checkout

--- README.md
+++ README.md
@@ -18,5 +18,20 @@ Agora passamos a ter 2 tabelas no banco, uma do Users e outra do Products.
 Na aula, ao invés de criar a rota para cadastrar o User, ensinou a criar usando o flask shell.
 "user = User(username="admin", password="12345")" para criar o user, depois "db.session.add(user)" e finalmente "db.session.commit()".
 
+Para autenticar, usa a lib Flask Login, com o modulo login_user, e LoginManager.
+No loginManager, estabelecemos a login.view, que é a rota de acesso para login que estabelecemos na api. E vai ser necessário uma secret key, que definimos a que quisermos e informamos num app.config['SECRET KEY'].
+Após efetuar o login, podemos ver no headers que foi criado um cookie de autenticação, e assim constatamos que funcionou.
+Finalmente, protegemos as rotas que quisermos com o login_required (@login_required).
+E, ainda, configurar uma função chamada user_loader. Assim, toda vez que fizermos uma requisição numa rota protegida pelo @login_required, o flask login vai precisar recuperar o usuário que está acessando a rota, e ele assim o fará consultando o @user_loader.
+
+Configuramos agora também a rota de logout, usando o logout_user da mesma lib.
+
+- Próximo passo é criar o carrinho de compras, que será um banco de dados que receberá dados dos modelos Products e do Users.
+Nessa tabela de checkout, vamos associar o id do model user, com o método ForeingKey, que fará refer6encia a chave do model User e Products.
+E vamos modificar o modelo de User para incluir o cart (carrinho) fazendo uma associação com a tebela CartItem.
+A propriedade backref serve para associar um item ao carrinho, e através do user, consegue recuperar o item do cart.
+Por fim, o método lazy é para que não sejam recuperados a cada vez que carregar/recuperar informação do user, como para checar autenticação, todos os objetos do carrinho, o que causaria lentidão no processo quando houver uma quantidade muito grande de itens. Assim, o lazy fará com que as informações referentes aos itens no carrinho sejam carregadas apenas quando o user acessar algum processo específico que precise carregar estas informações.
+
+
 
 2.33.00
--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-
 #definir caminho do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+# chave secreta exigida pelo LoginManager
+app.config['SECRET_KEY'] = 'minha_chave_secreta'
+# lib para administrar autenticacao login
+login_manager = LoginManager()
 #iniciar conexao com o banco de dados
 db = SQLAlchemy(app)
+# configuracao da lib login
+login_manager.init_app(app)
+# no login_view configuramos a rota que estabelecemos para login
+login_manager.login_view = 'login'
 
 #Modelagem do banco em linhas e colunas
 #Modelagem Usuarios
@@ -29,18 +36,57 @@ class Product(db.Model):
   # a description usamos um tipo Texto, que nao tem limitacao de tamanho
   description = db.Column(db.Text, nullable=True)
 
+# modelagem do carrinho - checkout
+class CartItem(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+  
+
+# Autenticação
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+# rotas de login
+@app.route('/login', methods=['POST'])
+def login():
+  data = request.json
+  user = User.query.filter_by(username=data.get('username')).first()
+
+  # if user:
+  #   if data.get('password') == user.password:
+  #     return jsonify({"message": "Logged, nice!"})
+  #   return jsonify({"message": "Not authorized acess, blah..."}), 401
+  # return jsonify({"message": "User not found"})
+  if user and data.get('password') == user.password:
+    # flask_login
+    login_user(user)
+    return jsonify({"message": "Login OK"})
+  return jsonify({"message": "Credentials Unauthorized..."})
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logout successfully..."})
+
+# rotas dos produtos
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
   data = request.json
   if 'name' in data and 'price' in data:
-    product = Product(name=data["name"], price=data["price"], description=data.get("description", "valor default se nao informarem nada"))
+    product = Product(name=data["name"], price=data["price"], description=data.get("description", "ou aqui vai o valor default se nao informarem nada"))
     db.session.add(product)
     db.session.commit()
     return jsonify({"message":"Produto cadastrado OK"}), 200
   # se o post nao informar o nome e preco, retorna msg de erro 400
   return jsonify({"message":"product data is invalid or not informed"}), 400
 
+# rota para delete product
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+@login_required
 def delete_product(product_id):
   # recupera o produto buscando pelo id
   product = Product.query.get(product_id)
@@ -66,6 +112,7 @@ def get_product_details(product_id):
   
 # modifica item
 @app.route('/api/products/<int:product_id>', methods=['PUT'])
+@login_required
 def update_product(product_id):
   product = Product.query.get(product_id)
   
